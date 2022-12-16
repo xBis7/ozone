e442cfe3d8ec67dd83455d2b61a641d523e325c6
xBis7 <christosbis@gmail.com>
2022-12-16 10:13:50 +0200

    omReq

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/S3IdentityProvider.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/S3IdentityProvider.java
@@ -2,10 +2,9 @@ package org.apache.hadoop.ozone.om;
 
 import org.apache.hadoop.ipc.IdentityProvider;
 import org.apache.hadoop.ipc.Schedulable;
+import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.S3Authentication;
 import org.apache.hadoop.security.UserGroupInformation;
-
-import java.util.Objects;
 
 public class S3IdentityProvider implements IdentityProvider {
 
@@ -14,12 +13,14 @@ public class S3IdentityProvider implements IdentityProvider {
 
   @Override
   public String makeIdentity(Schedulable schedulable) {
-    if (Objects.isNull(OzoneManager.getS3Auth())) {
+    OzoneManagerProtocolProtos.OMRequest omRequest =
+        OzoneManagerProtocolProtos.OMRequest.getDefaultInstance();
+    if (omRequest.hasS3Authentication()) {
+      S3Authentication s3Auth = omRequest.getS3Authentication();
+      return s3Auth.getAccessId();
+    } else {
       UserGroupInformation ugi = schedulable.getUserGroupInformation();
       return ugi == null ? null : ugi.getShortUserName();
-    } else {
-      S3Authentication s3Auth = OzoneManager.getS3Auth();
-      return s3Auth.getAccessId();
     }
   }
 }
