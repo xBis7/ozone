bf0988989b40122d94d987d6871b98fbfa47506f
xBis7 <christosbis@gmail.com>
2023-01-18 18:21:25 +0200

    hadoop code - checkstyle, findbugs fixed

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/callQueue/package-info.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/callQueue/package-info.java
@@ -6,40 +6,17 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.ozone.om;
-
-import org.apache.hadoop.ipc.IdentityProvider;
-import org.apache.hadoop.ipc.Schedulable;
-import org.apache.hadoop.security.UserGroupInformation;
-
-import java.io.IOException;
 
 /**
- * Ozone implementation of IdentityProvider used by
- * Hadoop DecayRpcScheduler.
+ * Ozone-side Hadoop CallQueue related classes.
  */
-public class OzoneIdentityProvider implements IdentityProvider {
-
-  public OzoneIdentityProvider() {
-  }
-
-  @Override
-  public String makeIdentity(Schedulable schedulable) {
-    UserGroupInformation ugi;
-    try {
-      ugi = UserGroupInformation.getCurrentUser();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    return ugi == null ? null : ugi.getShortUserName();
-  }
-}
+package org.apache.hadoop.ozone.callQueue;
