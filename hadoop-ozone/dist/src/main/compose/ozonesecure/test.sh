0504d87da438e8b865bcd99492b59e3455e819b0
xBis7 <christosbis@gmail.com>
2023-05-02 17:00:15 +0300

    cleanup

--- hadoop-ozone/dist/src/main/compose/ozonesecure/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure/test.sh
@@ -55,10 +55,10 @@ execute_robot_test scm -v container:1 -v count:2 replication/wait.robot
 docker-compose up -d --scale datanode=3
 execute_robot_test scm -v container:1 -v count:3 replication/wait.robot
 
-# stop om and s3g to free bound ports
+# stop om and s3g to free up bound ports
 stop_containers om s3g
 
-# start container services with FCQ config
+# start om and s3g containers with FCQ config
 docker-compose up -d om_fcq
 docker-compose up -d s3g_fcq
 
