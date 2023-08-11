7fac2b088cf9235b655b9c65c24bc5923296f345
Christos Bisias <christosbis@gmail.com>
2023-08-11 16:04:09 +0300

    fcq testing

--- hadoop-ozone/dist/src/main/compose/ozonesecure/fcq_testing.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure/fcq_testing.sh
@@ -33,6 +33,7 @@ writeKeys() {
   # This will be "om", "dn", "scm".
   node=$3
   iterations=$4
+  sleep_secs=$5
 
   container="$node"
   keytab_name="$node"
@@ -49,7 +50,7 @@ writeKeys() {
     docker exec ozonesecure-"$container"-1 kinit -kt /etc/security/keytabs/testuser.keytab testuser/"$keytab_name"@EXAMPLE.COM
     docker exec ozonesecure-"$container"-1 ozone freon omkg -t "$threads" -n "$keys" -v vol1 -b bucket1
 
-    sleep 1
+    sleep "$sleep_secs"
 
     counter=$(($counter+1))
 
@@ -57,20 +58,57 @@ writeKeys() {
   done
 }
 
-#writeKeysS3G() {
-#
-#}
+configureS3Cred() {
+  getsecret=$(docker-compose exec -T s3g ozone s3 getsecret)
+
+  awsAccessKey=$($getsecret | grep 'awsAccessKey' | awk -F '[=]' '{ print $2 }')
+  awsSecret=$($getsecret | grep 'awsSecret' | awk -F '[=]' '{ print $2 }')
+
+  docker-compose exec -T s3g export AWS_ACCESS_KEY="$awsAccessKey" AWS_SECRET_KEY="$awsSecret"
+
+  docker-compose exec -T s3g aws configure set default.s3.signature_version s3v4
+  docker-compose exec -T s3g aws configure set aws_access_key_id "$awsAccessKey"
+  docker-compose exec -T s3g aws configure set aws_secret_access_key "$awsSecret"
+  docker-compose exec -T s3g aws configure set region us-west-1
+}
+
+writeKeysS3G() {
+  threads=$1
+  keys=$2
+  iterations=$3
+  sleep_secs=$4
+
+  docker-compose exec -T s3g kinit -kt /etc/security/keytabs/testuser.keytab testuser/s3g@EXAMPLE.COM
+  getsecret=$(docker-compose exec -T s3g ozone s3 getsecret)
+
+  awsAccessKey=$($getsecret | grep 'awsAccessKey' | awk -F '[=]' '{ print $2 }')
+  awsSecret=$($getsecret | grep 'awsSecret' | awk -F '[=]' '{ print $2 }')
+
+  counter=0
+
+  while [[ $counter -lt $iterations ]]
+  do
+    docker-compose exec -e AWS_ACCESS_KEY="$awsAccessKey" -e AWS_SECRET_KEY="$awsSecret" s3g ozone freon s3bg -t "$threads" -n "$keys"
+    sleep "$sleep_secs"
+    counter=$(($counter+1))
+
+    echo "Finished iteration '$counter' on 's3g'"
+  done
+}
 
 # 1 user on the OM writing 1000 keys, sleeping 1 sec and then repeating.
-writeKeys 100 1000 om 1000 &
+# threads, keys, node, iterations, sleep
+writeKeys 100 1000 om 1000 1 &
 pid_om=$!
 
 # 1 user on the datanode writing 1.000.000 keys, sleeping 1 sec and then repeating.
-writeKeys 100 1000000 dn 1000 &
+# threads, keys, node, iterations, sleep
+writeKeys 100 100000 dn 1000 5 &
 pid_dn=$!
 
 # 1 user on the S3G writing 100.000 keys, sleeping 1 sec and then repeating.
-# writeKeysS3G 100 100000 1000 &
-# pid_s3g=$!
+# threads, keys, node, iterations, sleep
+writeKeysS3G 100 10000 1000 3 &
+pid_s3g=$!
 
-wait $pid_om $pid_dn
+#wait $pid_om $pid_dn
