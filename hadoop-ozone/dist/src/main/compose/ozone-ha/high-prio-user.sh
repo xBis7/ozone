1e3038cd5372619048a496bb69c0742014be832d
Christos Bisias <christosbis@gmail.com>
2023-09-25 20:26:28 +0300

    fcq testing under ozone-ha

---
+++ hadoop-ozone/dist/src/main/compose/ozone-ha/high-prio-user.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#suite:HA-unsecure
+
+COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export COMPOSE_DIR
+
+export SECURITY_ENABLED=false
+export OZONE_REPLICATION_FACTOR=3
+export SCM=scm1
+export OM_SERVICE_ID=omservice
+
+# shellcheck source=/dev/null
+source "$COMPOSE_DIR/../testlib.sh"
+
+iterations=$1
+
+counter=0
+while [[ $counter -lt $iterations ]]
+do
+  docker exec ozone-ha-datanode-1 ozone freon omkg -t 2 -n 10
+
+  sleep 3
+
+  counter=$(($counter+1))
+
+  echo "Finished iteration '$counter'."
+done
