9a71838034c5994aa4d9dff33adc4949f6ca1398
Christos Bisias <christosbis@gmail.com>
2023-11-06 18:29:14 +0200

    test script

--- hadoop-ozone/dist/src/main/compose/ozone-topology/exp.sh
+++ hadoop-ozone/dist/src/main/compose/ozone-topology/exp.sh
@@ -19,11 +19,21 @@ export OZONE_REPLICATION_FACTOR=3
 
 docker-compose up -d
 
-docker-compose exec -T om ozone freon ockg -t 10 -n 10
+docker-compose exec -T scm ozone sh volume create /vol1
+docker-compose exec -T scm ozone sh bucket create /vol1/bucket1
+docker-compose exec -T scm ozone sh key put /vol1/bucket1/key1 /etc/hosts
 
-docker-compose stop datanode_1
-docker-compose stop datanode_2
-docker-compose stop datanode_3
+docker-compose exec -T scm ozone admin scm roles
+docker-compose exec -T scm ozone admin datanode list
+docker-compose exec -T scm ozone admin container info 1
 
-docker-compose exec -T om ozone admin container report
+echo "ozone admin datanode decommission -id=scmservice --scm=10.5.0.71:9894 10.5.0.5/ozone-topology-datanode_2-1.ozone-topology_net"
+
+docker compose exec scm bash
+
+#docker-compose stop datanode_1
+#docker-compose stop datanode_2
+#docker-compose stop datanode_3
+#
+#docker-compose exec -T om ozone admin container report
 
